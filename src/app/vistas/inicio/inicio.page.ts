8c135a770c5bad42932ef436f5a5f6e5789abe41
Edgardo Cayo <edgardo.cayo.miranda@cftmail.cl>
2021-05-28 22:08:04 -0400

    uso de ion list en la pagina de inicio

--- src/app/vistas/inicio/inicio.page.ts
+++ src/app/vistas/inicio/inicio.page.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icono: string,
+  nombre: string,
+  direccion: string,
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icono: "bandage-outline",
+      nombre: "Nosotros",
+      direccion: "/nosotros",      
+    },
+    {
+      icono: "football-outline",
+      nombre: "Contacto",
+      direccion: "/contacto",
+    },
+  ];
 
   constructor() { }
 
